658e13ed407fbe6646641c1aec621ef8638b154f
Ajmwagar <ajmw.subs@gmail.com>
2017-10-09 00:31:19 -0700

    fix revert

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 var express = require('express');
 var app = express();
-var Poet = require('poet');
 var path = require('path');
 
-var poet = Poet(app, {
-  postPerPage: 5,
-  posts: __dirname + '/_posts',
-  metaFormat: 'json',
-});
 
-poet.watch()
-poet.init(app, function(){});
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
 app.use('/public', express.static('public'));
 
 app.get('/', function (req, res) {
