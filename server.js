a89dcbd8e1f4a43058be12b6d4cbc702cd0a5237
Ajmwagar <ajmw.subs@gmail.com>
2017-10-09 01:20:15 -0700

    added poet

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var Poet = require('poet');
+
+var poet = Poet(app, {
+  posts: '/_posts',
+  postsPerPage: 5,
+  metaFormat: 'json'
+});
+
+poet.init(app, function (){});
 
 
 app.use('/public', express.static('public'));
